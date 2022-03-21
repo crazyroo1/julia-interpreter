33135033fd097762845c51dbe3a8d970502499ba
Turner Eison <turner@turnereison.com>
2022-03-21 16:41:51 -0400

    Added documentation for the interpreter

--- Interpreter/Interpreter/Interpreter.swift
+++ Interpreter/Interpreter/Interpreter.swift
@@ -8,8 +8,18 @@
 import Foundation
 
 class Interpreter {
+    /// The symbol table for the interpreter.
     private var symbols: [String: Symbol] = [:]
     
+    
+    /// Interprets a parse tree
+    ///
+    /// - parameter parseTree: The head of the parse tree that should be interpreted
+    /// - parameter shouldPrint: Controls if the symbol table will be dumped to the
+    ///   console after successful interpretation
+    ///
+    /// This empties the symbol table first, then begins interpretation. This is to
+    /// ensure a fresh table for each interpretation.
     func interpret(parseTree: Program, shouldPrint: Bool = false) {
         // reset the symbol table on each interpret call
         symbols = [:]
@@ -24,10 +34,13 @@ class Interpreter {
         }
     }
     
+    /// Handles a block by handling each statement individually.
     private func handle(_ block: Block) {
         block.statements.forEach(handle)
     }
     
+    /// Handles a single statement by switching over all possible
+    /// statement types and handling that type in its own function.
     private func handle(_ statement: Statement) {
         switch statement {
         case .ifStatement(let ifStatement):
@@ -43,6 +56,8 @@ class Interpreter {
         }
     }
     
+    /// Handles an if statement by executing the expression, then
+    /// conditionally handling the `true` or `else` block appropriately.
     private func handle(_ ifStatement: IfStatement) {
         if handle(ifStatement.expression) {
             handle(ifStatement.trueBlock)
@@ -51,26 +66,41 @@ class Interpreter {
         }
     }
     
+    /// Assigns the result of the expression to the appropriate key in the symbol table.
     private func handle(_ assignmentStatement: AssignmentStatement) {
         symbols[assignmentStatement.identifier] = .variable(handle(assignmentStatement.expression))
     }
     
+    /// Handles a while statement as expected
+    ///
+    /// No surprises here. Evaluates the expression before each loop, then
+    /// handles the block of code in a standard Swift `while` loop.
     private func handle(_ whileStatement: WhileStatement) {
         while handle(whileStatement.expression) {
             handle(whileStatement.block)
         }
     }
     
+    /// Prints the result of evaluating the given expression.
     private func handle(_ printStatement: PrintStatement) {
         print(handle(printStatement.arithmeticExpression))
     }
     
+    /// Handles a repeat statement as expected
+    ///
+    /// No surprises here. Evaluates the expression after each loop, then
+    /// handles the block of code in a standard Swift `repeat` loop.
     private func handle(_ repeatStatement: RepeatStatement) {
         repeat {
             handle(repeatStatement.block)
         } while handle(repeatStatement.untilExpression)
     }
     
+    /// Evaluates a boolean expression
+    ///
+    /// First, the value of `lhs` and `rhs` is evaluated. Then,
+    /// the `relativeOperator` is switched on, and the result of the
+    /// comparison is returned to the caller.
     private func handle(_ expression: BooleanExpression) -> Bool {
         let lhs = handle(expression.lhs)
         let rhs = handle(expression.rhs)
@@ -91,11 +121,22 @@ class Interpreter {
         }
     }
     
+    /// Handles an arithmetic expression
+    ///
+    /// Breaks down the expression first.
+    ///
+    /// - If the value is an identifier, try to return the value from the symbol table.
+    ///   If said value doesn't exist, `0` is returned as the default.
+    /// - If the value is an `integerLiteral`, the value is returned directly.
+    /// - If the value is an expression, the operator is switched on. Both sides of the expression
+    ///   are evaluated individually, and their results are combined with the switched operator.
     private func handle(_ expression: ArithmeticExpression) -> Int {
         switch expression {
         case .identifier(let id):
             let value = symbols[id] ?? .variable(0)
-            guard case .variable(let intValue) = value else { fatalError() }
+            guard case .variable(let intValue) = value else {
+                fatalError("Tried to get the value of a function symbol, which is illegal")
+            }
             return intValue
             
         case .integerLiteral(let int):
--- Interpreter/Interpreter/Symbol.swift
+++ Interpreter/Interpreter/Symbol.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
+/// The types of symbols allowed in the interpreter
+///
+/// A symbol can either be for a function or for a variable.
+///
+/// A function symbol looks like this in Julia, where `a` is the symbol
+/// ``` julia
+/// function a() // <----- `a` is the symbol
+///     // do stuff
+/// end
+/// ```
+///
+/// A variable symbol is any other symbol located in the program.
+/// For example, a symbol first seen in an assignment statement looks like this,
+/// where `x` is the symbol
+/// ``` julia
+/// x = 5 // <----- `x` is the symbol
+/// ```
+///
 enum Symbol {
     case function
     case variable(Int)
